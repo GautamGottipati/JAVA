6c513544ef0ff249313be692baff946584c18aa6
Gottipati Gautam <38732642+GautamGottipati@users.noreply.github.com>
2020-01-13 18:12:50 +0530

    Update AbstractDemo.java

--- abstract/AbstractDemo.java
+++ abstract/AbstractDemo.java
@@ -1,7 +1,7 @@
 public class AbstractDemo {
 
    public static void main(String [] args) {
-      Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
+      Salary s = new Salary("Gautam", "Hyderabad, Telangana", 3, 3600.00);
       Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
       System.out.println("Call mailCheck using Salary reference --");
       s.mailCheck();
